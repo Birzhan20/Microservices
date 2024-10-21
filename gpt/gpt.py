043ef1ca7553212962349eae6f3c474cea441b6f
BIRZHAN <birzhan981@gmail.com>
2024-10-21 17:25:46 +0500

    gpt

---
+++ gpt/gpt.py
@@ -0,0 +1,40 @@
+import nest_asyncio
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import uvicorn
+import g4f
+
+nest_asyncio.apply()
+
+app = FastAPI()
+
+
+class Txt(BaseModel):
+    text: str | int
+
+
+@app.post("/gpt")
+async def generate_meta_description(request_body: Txt):
+    text = request_body.text
+    print(text)
+
+    if not text:
+        return JSONResponse(content={"error": "No input text provided"}, status_code=400)
+
+    try:
+        response = g4f.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": f"Создай уникальный мета-текст до 200 символов: {text}"}],
+            stream=False,
+        )
+        print(response)
+
+        return JSONResponse(content={"meta": response})
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8011)
