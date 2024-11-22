3eb973db84616878618162729f598cc759c525d9
BIRZHAN <birzhan981@gmail.com>
2024-11-22 10:40:41 +0500

    remade topics and docker connections

--- create_topics/topics.py
+++ create_topics/topics.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
-from kafka import KafkaAdminClient
-from kafka.admin import NewTopic
-import time
-import socket
+from kafka.admin import KafkaAdminClient, NewTopic
 
+# Инициализация клиента администратора Kafka
+admin_client = KafkaAdminClient(
+    bootstrap_servers="localhost:9092",  # Используйте внешний адрес, если скрипт не внутри контейнера
+    client_id='admin-client'  # Пример уникального идентификатора клиента
+)
 
-topics_list = [
+# Список существующих тем
+topic_names = [
     "category-index", "category-seo", "goods-category-result", "goods-index", "goods-seo",
     "d-a-categories-index", "d-a-categories-seo", "d-a-goods-index", "d-a-goods-seo", "d-a-result",
     "d-g-categories-index", "d-g-categories-seo", "d-g-goods-index", "d-g-goods-seo", "d-g-result",
@@ -16,42 +18,12 @@ topics_list = [
     "translation", "translation-res",
 ]
 
+# Преобразуем все строки в объекты NewTopic
+topic_list = [NewTopic(name=topic, num_partitions=1, replication_factor=1) for topic in topic_names]
 
-def create_topics(bootstrap_servers, topics):
-    admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+# Добавление новой темы
+new_topic = NewTopic(name="example_topic", num_partitions=1, replication_factor=1)
+topic_list.append(new_topic)
 
-    new_topics = [NewTopic(name=topic, num_partitions=3, replication_factor=2) for topic in topics]
-
-    try:
-        admin_client.create_topics(new_topics=new_topics, validate_only=False)
-        print("Topics created successfully.")
-    except Exception as e:
-        print(f"Error creating topics: {e}")
-    finally:
-        admin_client.close()
-
-
-def wait_for_kafka(bootstrap_servers, retries=30, delay=10):
-    """Функция для ожидания доступности Kafka"""
-    host, port = bootstrap_servers.split(":")
-    for _ in range(retries):
-        try:
-            sock = socket.create_connection((host, int(port)), timeout=1)
-            sock.close()
-            print(f"Kafka is up and running at {bootstrap_servers}")
-            return True
-        except (socket.timeout, socket.error):
-            print(f"Waiting for Kafka to be available at {bootstrap_servers}...")
-            time.sleep(delay)
-    return False
-
-
-if __name__ == "__main__":
-    kafka_address = 'kafka:9092'  # Используйте правильное имя сервиса или IP-адрес
-
-    # Ожидаем, пока Kafka не станет доступной
-    if wait_for_kafka(kafka_address):
-        create_topics(kafka_address, topics_list)
-        print("Topics created")
-    else:
-        print(f"Failed to connect to Kafka at {kafka_address}")
+# Создание тем в Kafka
+admin_client.create_topics(new_topics=topic_list, validate_only=False)
