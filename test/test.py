737cf8cf9c3a56ae36c5095d22d69869ee93eef6
birzhan <birzhan981@gmail.com>
2025-01-15 13:57:22 +0500

    senser set new host

--- test/test.py
+++ test/test.py
@@ -24,4 +24,4 @@ async def sender(msg: Message):
     print(f"Сообщение '{message}' отправлено в топик '{topic}'")
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
